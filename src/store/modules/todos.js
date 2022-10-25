f91e5e7d2c1b064d345be738ab461e49b384a0ad
“riverhuang-batam” <riverhuang011@gmail.com>
2022-10-25 18:32:42 +0700

    add todo axios post

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -22,10 +22,15 @@ const actions = {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
         console.log(response)
         commit('setTodos', response.data)
+    },
+    async addTodo({commit}, title){
+      const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false})
+      commit ('newTodo', response.data)
     }
 };
 const mutations = {
-  setTodos: (state, todos) => (state.todos = todos)
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo)
 };
 export default {
     state,
